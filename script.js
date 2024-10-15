e2e3396f6daa571dcbceda847f5f38594af6537d
Carlos Acosta <carlosaac23@hotmail.com>
2024-10-15 15:41:21 -0500

    Add the whole function to play a single round. Corrected some errors and improve the code

--- script.js
+++ script.js
@@ -5,29 +5,37 @@ function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
 
   if (computerChoice === 0) {
-    return 'Piedra';
+    return 'rock';
   } else if (computerChoice === 1) {
-    return 'Papel';
+    return 'paper';
   } else {
-    return 'Tijeras';
+    return 'scissors';
   }
 }
 
 function getHumanChoice() {
-  const humanChoice = prompt('Piedra, Papel o Tijeras! Listo!?');
-  return humanChoice;
+  const humanChoice = prompt('Rock, Paper or Scissors! Ready!? Go!');
+  return humanChoice.toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice === 'paper' && computerChoice === 'rock') {
-    console.log(`Ganaste! ${humanChoice} vence a ${computerChoice}`);
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
     humanScore++;
   } else {
-    console.log(`Perdiste! ${computerChoice} vence a ${humanChoice}`);
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
     computerScore++;
   }
 
-  console.log(`Puntaje del usuario: ${humanScore} -- Puntaje del computador: ${computerScore}`);
+  console.log(`User score: ${humanScore} -- Computer score: ${computerScore}`);
 }
 
 const humanSelection = getHumanChoice();
