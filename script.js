6e67ed36e924a483c514b93d11654ea79f8e07e3
Carlos Acosta <carlosaac23@hotmail.com>
2024-10-15 14:00:16 -0500

    Add a function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+
+  if (computerChoice === 0) {
+    console.log('Piedra');
+  } else if (computerChoice === 1) {
+    console.log('Papel');
+  } else {
+    console.log('Tijeras');
+  }
+}
