4020b18792c5084f4e7f737968e5b43b1f3125e2
Carlos Acosta <carlosaac23@hotmail.com>
2024-10-15 14:22:34 -0500

    Add a style file to change bg color. Add a function to get user choice and other function to play one round

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
 
   if (computerChoice === 0) {
-    console.log('Piedra');
+    return 'Piedra';
   } else if (computerChoice === 1) {
-    console.log('Papel');
+    return 'Papel';
   } else {
-    console.log('Tijeras');
+    return 'Tijeras';
   }
 }
+
+function getHumanChoice() {
+  const humanChoice = prompt('Piedra, Papel o Tijeras! Listo!?');
+  return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log(`Ganaste! ${humanChoice} vence a ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`Perdiste! ${computerChoice} vence a ${humanChoice}`);
+    computerScore++;
+  }
+
+  console.log(`Puntaje del usuario: ${humanScore} -- Puntaje del computador: ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
